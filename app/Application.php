7386026f025a561bb4fd3210fb2ca195b6aa949f
Douglas S. Green <douglas@nurd.site>
2023-06-25 20:16:22 -0400

    Adding plugin architecture

--- app/Application.php
+++ app/Application.php
@@ -2,10 +2,28 @@
 
 namespace PageMaker;
 
+// @todo Add plugin features to body builder
 class Application
 {
+    protected $plugins = [];
+
     public static function isCli(): bool
     {
         return php_sapi_name() === 'cli';
+    }
+
+    public function registerPlugin(string $id, BasePlugin $plugin) {
+        $this->plugins[$id][] = $plugin;
+    }
+
+    public function run() {
+        foreach ($this->plugins as $plugin) {
+            try {
+                $output = $plugin->render();
+            } catch (Exception $e) {
+                $output = "Error in " . $plugin->getName() . ": " . $e->getMessage();
+            }
+            // @todo Add to ID
+        }
     }
 }
