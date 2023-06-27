52cdd7be4282568cf1587b4bf5b6c2fab6642f8c
Douglas S. Green <douglas@nurd.site>
2023-06-27 06:10:33 -0400

    Rewriting email sender

--- app/EmailSender.php
+++ app/EmailSender.php
@@ -4,17 +4,38 @@ namespace PageMaker;
 
 class EmailSender
 {
-/**
- * Function to send an email with an attachment
- *
- * @param string $from - The email address sending the email
- * @param string $recipient - The recipient of the email
- * @param string $subject - The subject of the email
- * @param string $message - The main message of the email
- * @param string $attachmentFilePath - The file path to the attachment
- * @return array - An array with success or error message
- */
-    function sendEmailWithAttachment($from, $recipient, $subject, $message, $attachmentFilePath)
+    private $from;
+    private $recipient;
+    private $subject;
+    private $message;
+    private $attachmentFilePath;
+
+    public function __get($name)
+    {
+        return $this->$name;
+    }
+
+    public function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+
+    public function send() {
+        if ($this->attachmentFilePath) {
+            $this->sendEmailWithAttachment();
+        } elseif (preg_match('/<\w/', $this->message)) {
+            $this->sendHTMLEmail();
+        } else {
+            $this->sendPlainTextEmail();
+        }
+    }
+
+    /**
+     * Function to send an email with an attachment
+     *
+     * @return array - An array with success or error message
+     */
+    protected function sendEmailWithAttachment()
     {
         // Generate a unique separator for multipart content
         $separator = md5(microtime(true) . mt_rand());
@@ -23,7 +44,7 @@ class EmailSender
         $eol = "\r\n";
 
         // Construct the main headers (multipart is required for attachments)
-        $headers = 'From: ' . $from . $eol;
+        $headers = 'From: ' . $this->from . $eol;
         $headers .= 'MIME-Version: 1.0' . $eol;
         $headers .= 'Content-Type: multipart/mixed; boundary="' . $separator . '"' . $eol;
 
@@ -31,24 +52,63 @@ class EmailSender
         $body = '--' . $separator . $eol;
         $body .= 'Content-Type: text/plain; charset="UTF-8"' . $eol;
         $body .= 'Content-Transfer-Encoding: base64' . $eol;
-        $body .= chunk_split(base64_encode($message)) . $eol;
+        $body .= chunk_split(base64_encode($this->message)) . $eol;
 
         // Read and encode the attachment
-        $encodedAttachment = chunk_split(base64_encode(file_get_contents($attachmentFilePath)));
+        $encodedAttachment = chunk_split(base64_encode(file_get_contents($this->attachmentFilePath)));
 
         // Add attachment to the body
         $body .= '--' . $separator . $eol;
-        $body .= 'Content-Type: application/octet-stream; name="' . basename($attachmentFilePath) . '"' . $eol;
+        $body .= 'Content-Type: application/octet-stream; name="' . basename($this->attachmentFilePath) . '"' . $eol;
         $body .= 'Content-Transfer-Encoding: base64' . $eol;
-        $body .= 'Content-Disposition: attachment; filename="' . basename($attachmentFilePath) . '"' . $eol . $eol;
+        $body .= 'Content-Disposition: attachment; filename="' . basename($this->attachmentFilePath) . '"' . $eol . $eol;
         $body .= $encodedAttachment . $eol;
         $body .= '--' . $separator . '--';
 
         // Send the email and return success or error message
-        if (mail($recipient, $subject, $body, $headers)) {
+        if (mail($this->recipient, $this->subject, $body, $headers)) {
+            return array('success' => true);
+        } else {
+            return array('error' => error_get_last());
+        }
+    }
+
+   /**
+     * Function to send a plain text email with no attachment
+     *
+     * @return array - An array with success or error message
+     */
+    protected function sendPlainTextEmail()
+    {
+        $headers = 'From: ' . $this->from . "\r\n";
+        $headers .= 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-Type: text/plain; charset=UTF-8' . "\r\n";
+
+        // Send the email and return success or error message
+        if (mail($this->recipient, $this->subject, $this->message, $headers)) {
+            return array('success' => true);
+        } else {
+            return array('error' => error_get_last());
+        }
+    }
+
+    /**
+     * Function to send an HTML email with no attachment
+     *
+     * @return array - An array with success or error message
+     */
+    protected function sendHTMLEmail()
+    {
+        $headers = 'From: ' . $this->from . "\r\n";
+        $headers .= 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-Type: text/html; charset=UTF-8' . "\r\n";
+
+        // Send the email and return success or error message
+        if (mail($this->recipient, $this->subject, $this->message, $headers)) {
             return array('success' => true);
         } else {
             return array('error' => error_get_last());
         }
     }
 }
+
--- app/Registry.php
+++ app/Registry.php
@@ -1,6 +1,6 @@
 <?php
 
-class PageMaker;
+namespace PageMaker;
 
 class Registry
 {
