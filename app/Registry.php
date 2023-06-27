1a9cb58e7b44821d6db5ee944f9a1e643c7d5dda
Douglas S. Green <douglas@nurd.site>
2023-06-27 05:16:29 -0400

    Adding registry

---
+++ app/Registry.php
@@ -0,0 +1,40 @@
+<?php
+
+class PageMaker;
+
+class Registry
+{
+    private $registry = array();
+
+    public function set($key, $value)
+    {
+        if (!isset($this->registry[$key])) {
+            $this->registry[$key] = $value;
+        } else {
+            throw new Exception("There is already an entry for key $key");
+        }
+    }
+
+    public function get($key)
+    {
+        if (isset($this->registry[$key])) {
+            return $this->registry[$key];
+        } else {
+            throw new Exception("No entry for key $key");
+        }
+    }
+
+    public function delete($key)
+    {
+        if (isset($this->registry[$key])) {
+            unset($this->registry[$key]);
+        } else {
+            throw new Exception("No entry for key $key");
+        }
+    }
+
+    public function has($key)
+    {
+        return isset($this->registry[$key]);
+    }
+}
