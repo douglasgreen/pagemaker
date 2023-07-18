9f7d8c9275248829abc7caadd1f2ef554cac29ac
Douglas S. Green <douglas@nurd.site>
2023-07-18 06:19:12 -0400

    Adding controller interface

---
+++ app/Controllers/ControllerInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PageMaker;
+
+use PageMaker\Registry;
+use PageMaker\Request;
+
+interface ControllerInterface
+{
+    protected function setRegistry(Registry $registry): void;
+    protected function setRequest(Request $request): void;
+
+    protected function getRegistry(): Registry;
+    protected function getRequest(): Request;
+}
