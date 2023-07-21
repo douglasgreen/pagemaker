26e9c32dc43893435eea5b187d5369de3722cefa
Douglas S. Green <douglas@nurd.site>
2023-07-21 06:05:17 -0400

    Adding request interface

--- dev/app/Request/Request.php
+++ dev/app/Request/Request.php
@@ -35,66 +35,13 @@ class Request
     protected $file;
     protected $cookie;
 
-    /**
-     * @todo Remove default stripping of HTML tags
-     */
-    public function filterInput(array $input, string $key, $default)
-    {
-        return isset($input[$key]) ? filter_var($input[$key], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : $default;
-    }
-
     public function __construct()
     {
         global $argv;  // Access to the command line arguments
 
         // Check if the script is running in a command line interface (CLI) environment
         if (PhpConfig::isCli()) {
-            // Create an empty array to store our GET data, equivalent to the $_GET superglobal in a web environment
-            $this->get = [];
-
-            // This will hold the names of the options (command line arguments starting with '--')
-            $optionNames = [];
-
-            // Loop over each command line argument
-            foreach ($argv as $arg) {
-                // Using regex to check if the argument starts with '--' (i.e., it's a long option)
-                // If so, it captures the name (the part after '--') and stores it in $optionNames array
-                // If not, it adds false to $optionNames to keep the indexes in sync with $argv
-                if (preg_match('/^--(?<name>\w+)$/', $arg, $match)) {
-                    $optionNames[] = $match['name'];
-                } else {
-                    $optionNames[] = false;
-                }
-            }
-
-            // Counter for looping through arguments
-            $i = 0;
-            // Total number of arguments
-            $n = count($argv);
-
-            // Loop over the command line arguments
-            while ($i < $n) {
-                // Check if the current item in optionNames is not false (i.e., it is an option name)
-                if ($optionNames[$i] !== false) {
-                    $name = $optionNames[$i];
-
-                    // If the current argument is the last one, or the next argument is also an option name
-                    // (meaning, the current option doesn't have a value), we assign true to the current option
-                    // This indicates that the option was provided, but no specific value was given
-                    if (!isset($argv[$i + 1]) || $optionNames[$i + 1] !== false) {
-                        $value = true;  // The option is present but doesn't have a specific value
-                    } else {
-                        // Else, the value of the option is the next argument
-                        // Increase $i to skip the value in the next iteration
-                        $value = $argv[++$i];
-                    }
-
-                    // Add the option and its value to the get array
-                    $this->get[$name] = $value;
-                }
-                // Move on to the next argument
-                $i++;
-            }
+            $this->get = $this->processArgv();
         } else {
             // For a non-CLI environment, populate the relevant properties from the superglobals
             $this->get = $_GET;      // Contains all GET request parameters
@@ -144,5 +91,61 @@ class Request
     public function isGet(): bool
     {
         return $this->server('REQUEST_METHOD') === 'GET';
+    }
+
+    protected function filterInput(array $input, string $key, $default)
+    {
+        return isset($input[$key]) ? filter_var($input[$key], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : $default;
+    }
+
+    protected function processArgv(): array
+    {
+            // Create an empty array to store our GET data, equivalent to the $_GET superglobal in a web environment
+            $get = [];
+
+            // This will hold the names of the options (command line arguments starting with '--')
+            $optionNames = [];
+
+            // Loop over each command line argument
+        foreach ($argv as $arg) {
+            // Using regex to check if the argument starts with '--' (i.e., it's a long option)
+            // If so, it captures the name (the part after '--') and stores it in $optionNames array
+            // If not, it adds false to $optionNames to keep the indexes in sync with $argv
+            if (preg_match('/^--(?<name>\w+)$/', $arg, $match)) {
+                $optionNames[] = $match['name'];
+            } else {
+                $optionNames[] = false;
+            }
+        }
+
+            // Counter for looping through arguments
+            $i = 0;
+            // Total number of arguments
+            $n = count($argv);
+
+            // Loop over the command line arguments
+        while ($i < $n) {
+            // Check if the current item in optionNames is not false (i.e., it is an option name)
+            if ($optionNames[$i] !== false) {
+                $name = $optionNames[$i];
+
+                // If the current argument is the last one, or the next argument is also an option name
+                // (meaning, the current option doesn't have a value), we assign true to the current option
+                // This indicates that the option was provided, but no specific value was given
+                if (!isset($argv[$i + 1]) || $optionNames[$i + 1] !== false) {
+                    $value = true;  // The option is present but doesn't have a specific value
+                } else {
+                    // Else, the value of the option is the next argument
+                    // Increase $i to skip the value in the next iteration
+                    $value = $argv[++$i];
+                }
+
+                // Add the option and its value to the get array
+                $get[$name] = $value;
+            }
+            // Move on to the next argument
+            $i++;
+        }
+            return $get;
     }
 }
