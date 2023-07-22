db3f43cbd7693f49d97f9ec916c2831910f7c72b
Douglas S. Green <douglas@nurd.site>
2023-07-22 10:10:26 -0400

    Fixing request class

--- dev/app/Contract/Request.php
+++ dev/app/Contract/Request.php
@@ -2,59 +2,7 @@
 
 namespace PageMakerDev\Contract;
 
-use PageMakerDev\Utility\PhpConfig;
-
-/**
- * Handles retrieval of $_GET, $_POST, $_SERVER, and $_COOKIE vars from a request.
- *
- * $_REQUEST is not offered. Use $_GET and $_POST instead.
- *
- * @interface Request
- */
 interface Request
 {
-    /**
-     * Retrieves a value from the $_COOKIE superglobal based on a given key.
-     *
-     * @param string $key The key to look up in the $_COOKIE superglobal.
-     *
-     * @return mixed|null The value associated with the key, or null if the key is not set.
-     */
-    public function cookie(string $key);
-
-    /**
-     * Retrieves a value from the $_FILE superglobal based on a given key.
-     *
-     * @param string $key The key to look up in the $_FILE superglobal.
-     *
-     * @return mixed|null The value associated with the key, or null if the key is not set.
-     */
-    public function file(string $key);
-
-    /**
-     * Retrieves a value from the $_GET superglobal based on a given key.
-     *
-     * @param string $key The key to look up in the $_GET superglobal.
-     *
-     * @return mixed|null The value associated with the key, or null if the key is not set.
-     */
-    public function get(string $key);
-
-    /**
-     * Retrieves a value from the $_POST superglobal based on a given key.
-     *
-     * @param string $key The key to look up in the $_POST superglobal.
-     *
-     * @return mixed|null The value associated with the key, or null if the key is not set.
-     */
-    public function post(string $key);
-
-    /**
-     * Retrieves a value from the $_SERVER superglobal based on a given key.
-     *
-     * @param string $key The key to look up in the $_SERVER superglobal.
-     *
-     * @return mixed|null The value associated with the key, or null if the key is not set.
-     */
-    public function server(string $key);
+    public function get(string $type, string $key);
 }
--- dev/app/Request.php
+++ dev/app/Request.php
@@ -2,7 +2,6 @@
 
 namespace PageMakerDev;
 
-use PageMakerDev\Utility\PhpConfig;
 use PageMakerDev\Contract\Request as RequestInterface;
 
 /**
@@ -14,132 +13,100 @@ use PageMakerDev\Contract\Request as RequestInterface;
  * get, post, server, file, and cookie methods retrieve a value from the corresponding superglobal. If the key is not
  * present in the superglobal, they return a default value.
  *
- * The filter_var function with the FILTER_SANITIZE_FULL_SPECIAL_CHARS filter may not be appropriate for all
- * situations. For example, you may need different sanitization for email addresses, URLs, integers, etc.
- *
  * For testing, fake superglobal arrays can be injected to the constructor. Otherwise, the built-in superglobals are
  * accessed directly rather than being copied to save memory.
  */
 class Request implements RequestInterface
 {
-    protected $getVars;
-    protected $postVars;
-    protected $serverVars;
-    protected $cookies;
-    protected $files;
+    protected $data = [
+        'arg' => null,
+        'cookie' => null,
+        'file' => null,
+        'get' => null,
+        'post' => null,
+        'server' => null,
+    ];
 
-    public function __construct(
-        ?array $getVars = null,
-        ?array $postVars = null,
-        ?array $serverVars = null,
-        ?array $files = null,
-        ?array $cookies = null,
-    ) {
-        global $argv;  // Access to the command line arguments
-
-        // Check if the script is running in a command line interface (CLI) environment
-        if (PhpConfig::isCli()) {
-            $this->getVars = $this->processArgv();
-        } else {
-            // For a non-CLI environment, populate the relevant properties from the superglobals
-            $this->getVars = $getVars;
-            $this->postVars = $postVars;
-            $this->serverVars = $serverVars;
-            $this->files = $files;
-            $this->cookies = $cookies;
+    public function set(string $type, array $data): void
+    {
+        if (!array_key_exists($type, $this->data)) {
+            throw new LibraryException('Invalid type');
         }
+
+        if ($type == 'arg') {
+            $this->data[$type] = $this->processArgv($data);
+        } else {
+            $this->data[$type] = $data;
+        }
+    }
+
+    public function get(string $type, string $key, $default = null)
+    {
+        if (!array_key_exists($type, $this->data)) {
+            throw new LibraryException('Invalid type');
+        }
+
+        switch ($type) {
+            case 'arg':
+                if (isset($this->data['arg'])) {
+                    $value = $this->data['arg'][$key] ?? $default;
+                } else {
+                    global $argv;
+                    $this->data['arg'] = $this->processArgv($data);
+                    $value = $this->data['arg'][$key] ?? $default;
+                }
+                break;
+            case 'cookie':
+                if (isset($this->data['cookie'])) {
+                    $value = $this->data['cookie'][$key] ?? $default;
+                } else {
+                    $value = $_COOKIE[$key] ?? $default;
+                }
+                break;
+            case 'get':
+                if (isset($this->data['get'])) {
+                    $value = $this->data['get'][$key] ?? $default;
+                } else {
+                    $value = $_GET[$key] ?? $default;
+                }
+                break;
+            case 'file':
+                if (isset($this->data['file'])) {
+                    $value = $this->data['file'][$key] ?? $default;
+                } else {
+                    $value = $_FILES[$key] ?? $default;
+                }
+                break;
+            case 'post':
+                if (isset($this->data['post'])) {
+                    $value = $this->data['post'][$key] ?? $default;
+                } else {
+                    $value = $_POST[$key] ?? $default;
+                }
+                break;
+            case 'server':
+                if (isset($this->data['server'])) {
+                    $value = $this->data['server'][$key] ?? $default;
+                } else {
+                    $value = $_SERVER[$key] ?? $default;
+                }
+                break;
+        }
+        return $value;
     }
 
     /**
-     * This is just a reader. Update cookie with Cookie handler.
+     * Process only long-named arguments from the argv array, equivalent to the $_GET superglobal in a web environment.
      */
-    public function cookie(string $key, $default = null, $useFilter = false)
+    protected function processArgv($argv): array
     {
-        if (isset($this->cookies)) {
-            $value = $this->cookies[$key] ?? $default;
-        } else {
-            $value = $_COOKIE[$key] ?? $default;
-        }
+        // Create an empty array to store our GET data, equivalent to the $_GET superglobal in a web environment
+        $args = [];
 
-        if ($useFilter) {
-            $value = filter_var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        }
+        // This will hold the names of the options (command line arguments starting with '--')
+        $optionNames = [];
 
-        return $value;
-    }
-
-    public function get(string $key, $default = null, $useFilter = false)
-    {
-        if (isset($this->getVars)) {
-            $value = $this->getVars[$key] ?? $default;
-        } else {
-            $value = $_GET[$key] ?? $default;
-        }
-
-        if ($useFilter) {
-            $value = filter_var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        }
-
-        return $value;
-    }
-
-    /**
-     * This is just a reader. Process the result with file Uploader.
-     */
-    public function file(string $key, $default = null, $useFilter = false)
-    {
-        if (isset($this->files)) {
-            $value = $this->files[$key] ?? $default;
-        } else {
-            $value = $_FILES[$key] ?? $default;
-        }
-
-        if ($useFilter) {
-            $value = filter_var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        }
-
-        return $value;
-    }
-
-    public function post(string $key, $default = null, $useFilter = false)
-    {
-        if (isset($this->postVars)) {
-            $value = $this->postVars[$key] ?? $default;
-        } else {
-            $value = $_POST[$key] ?? $default;
-        }
-
-        if ($useFilter) {
-            $value = filter_var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        }
-
-        return $value;
-    }
-
-    public function server(string $key, $default = null, $useFilter = false)
-    {
-        if (isset($this->serverVars)) {
-            $value = $this->serverVars[$key] ?? $default;
-        } else {
-            $value = $_SERVER[$key] ?? $default;
-        }
-
-        if ($useFilter) {
-            $value = filter_var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        }
-
-        return $value;
-    }
-
-    protected function processArgv(): array
-    {
-            // Create an empty array to store our GET data, equivalent to the $_GET superglobal in a web environment
-            $get = [];
-
-            // This will hold the names of the options (command line arguments starting with '--')
-            $optionNames = [];
-
-            // Loop over each command line argument
+        // Loop over each command line argument
         foreach ($argv as $arg) {
             // Using regex to check if the argument starts with '--' (i.e., it's a long option)
             // If so, it captures the name (the part after '--') and stores it in $optionNames array
@@ -151,12 +118,12 @@ class Request implements RequestInterface
             }
         }
 
-            // Counter for looping through arguments
-            $i = 0;
-            // Total number of arguments
-            $n = count($argv);
+        // Counter for looping through arguments
+        $i = 0;
+        // Total number of arguments
+        $n = count($argv);
 
-            // Loop over the command line arguments
+        // Loop over the command line arguments
         while ($i < $n) {
             // Check if the current item in optionNames is not false (i.e., it is an option name)
             if ($optionNames[$i] !== false) {
@@ -174,11 +141,11 @@ class Request implements RequestInterface
                 }
 
                 // Add the option and its value to the get array
-                $get[$name] = $value;
+                $args[$name] = $value;
             }
             // Move on to the next argument
             $i++;
         }
-            return $get;
+        return $args;
     }
 }
